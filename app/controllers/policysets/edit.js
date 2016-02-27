b50b0b9a52956b673eaec23afd4545c0acf4642f
galmi <galmi.ru@gmail.com>
2016-02-27 13:10:52 +0700

    added actions

--- app/controllers/policysets/edit.js
+++ app/controllers/policysets/edit.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    actions: {
+        commit(policySet) {
+            policySet.save();
+            this.transitionToRoute('policysets');
+        },
+        rollback(policySet) {
+            policySet.rollback();
+            this.transitionToRoute('policysets');
+        }
+    }
 });
